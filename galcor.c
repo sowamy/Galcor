038fb24a99e793f84f6109ec6335efbe152ba4df
Sowamy <adlefevers2@yahoo.com>
2017-03-27 17:21:41 -0500

    Create galcor.c

---
+++ galcor.c
@@ -0,0 +1,15 @@
+float sum_array( const int *array_in, int size );
+
+float sum_array( const int *array_in, int size )
+{
+    float sum = 0;
+    int counter = 0;
+    int *arrayPtr = array_in;
+
+    for( counter = 0; counter < size; counter++ ) {
+        sum += *arrayPtr;
+        arrayPtr++;
+    }
+
+    return sum;
+}
