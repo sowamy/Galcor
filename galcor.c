7502cf640fc1f27b98483935ea45127cc9f801fc
Sowamy <adlefevers2@yahoo.com>
2017-03-28 10:01:48 -0500

    Update galcor.c

--- galcor.c
+++ galcor.c
@@ -1,5 +1,7 @@
 float g_SumArray( const int *array_in, int size );
 float g_AvgArray( const int *array_in, int size );
+void g_IntInsertionSort( int *inputArrayPtr, int size );
+void g_FloatInsertionSort( int *inputArrayPtr, int size );
 
 float g_SumArray( const int *array_in, int size )
 {
@@ -27,3 +29,65 @@ float g_AvgArray( const int *array_in, int size )
 
     return avg;
 }
+
+void g_IntInsertionSort( int *inputArrayPtr, int size )
+{
+    int *tempPtr = inputArrayPtr;
+    int a[ size ];         // Temporary array to do the sort on
+
+    int i = 0;
+    int j = 0;
+    int key = 0;
+
+    for( i = 0; i < size; i++ ) {
+        a[ i ] = *tempPtr++;
+    }
+
+    for( j = 1; j < 10; j++ ) {
+        key = a[ j ];
+        i = j - 1;
+        while( ( i >= 0 ) && ( a[ i ] > key ) ) {
+            a[ i + 1 ] = a[ i ];
+            i = i - 1;
+        }
+        a[ i + 1 ] = key;
+    }
+
+    i = 0;
+    tempPtr = inputArrayPtr;
+
+    for( i = 0; i < 10; i++ ) {
+        *tempPtr++ = a[ i ];
+    }
+} // END FUNCTION g_IntInsertionSort
+
+void g_FloatInsertionSort( int *inputArrayPtr, int size )
+{
+    int *tempPtr = inputArrayPtr;
+    int a[ size ];         // Temporary array to do the sort on
+
+    int i = 0;
+    int j = 0;
+    float key = 0;
+
+    for( i = 0; i < size; i++ ) {
+        a[ i ] = *tempPtr++;
+    }
+
+    for( j = 1; j < 10; j++ ) {
+        key = a[ j ];
+        i = j - 1;
+        while( ( i >= 0 ) && ( a[ i ] > key ) ) {
+            a[ i + 1 ] = a[ i ];
+            i = i - 1;
+        }
+        a[ i + 1 ] = key;
+    }
+
+    i = 0;
+    tempPtr = inputArrayPtr;
+
+    for( i = 0; i < 10; i++ ) {
+        *tempPtr++ = a[ i ];
+    }
+} // END FUNCTION g_FloatInsertionSort
