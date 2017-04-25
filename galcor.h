f4a719b1d03f44d678bc6b8c8a89055664317aa0
Sowamy <adlefevers2@yahoo.com>
2017-04-25 13:28:49 -0500

    Create galcor.h

---
+++ galcor.h
@@ -0,0 +1,7 @@
+#ifndef GALCOR_H
+#define GALCOR_H
+float g_Sum( const int *array_in, int size );
+float g_Average( const int *array_in, int size );
+void g_IntInsertionSort( int *inputArrayPtr, int size );
+void g_FloatInsertionSort( int *inputArrayPtr, int size );
+#endif // GALCOR_H
