a49464336dc6ca7a8524a910575875ff1c9b524c
Sowamy <adlefevers2@yahoo.com>
2017-04-29 10:54:58 -0500

    Update gDSP.h

--- gDSP.h
+++ gDSP.h
@@ -1,4 +1,6 @@
 #ifndef GDSP_H
 #define GDSP_H
-int *gDSP_DCSignalGenerator( size_t size, float amplitude, int delay );
+float *gDSP_DCSignalGenerator( size_t size, float amplitude, int delay );
+float *gDSP_ImpulseSignalGenerator( int size, float amplitude, int delay );
+int *gDSP_RandomIntegerSignalGenerator( int size, int minRange, int maxRange );
 #endif // GDSP_H
