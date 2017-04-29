8e5e16293f267372a8ce3eae6dd20f6b71408b48
Sowamy <adlefevers2@yahoo.com>
2017-04-29 10:54:19 -0500

    Update gDSP.c

--- gDSP.c
+++ gDSP.c
@@ -1,20 +1,5 @@
 //-----------------------------------------------------------------------------------------------------------
-// FILE: 							  		gDSP.c
-// DESCRIPTION: Contains a multitude of Digital Signal Processing functionalities
-//-----------------------------------------------------------------------------------------------------------
-#include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
-//-----------------------------------------------------------------------------------------------------------
-// Function Prototypes
-double *gDSP_DCSignalGenerator( int size, double amplitude, int delay );
-//-----------------------------------------------------------------------------------------------------------
-/* FUNCTION: gDSP_DCSignalGenerator
-// DESCRIPTION:
-// -- Generates a constant DC signal of length <samples>
-// -- Returns pointer to the location of the generated DC signal
-// ARGUMENTS://-----------------------------------------------------------------------------------------------------------
-// FILE: 							  		gDSP.c
+// FILE: 			gDSP.c
 // DESCRIPTION: Contains a multitude of Digital Signal Processing functionalities
 //-----------------------------------------------------------------------------------------------------------
 #include <stdio.h>
@@ -147,41 +132,4 @@ int *gDSP_RandomIntegerSignalGenerator( int size, int minRange, int maxRange )
 
     return sourcePtr;
 } // END FUNCTION gDSP_RandomIntegerSignalGenerator
-//-----------------------------------------------------------------------------------------------------------
-
-// -- samples           := Number of samples of the full signal to be created and returned
-// -- amplitude         := The amplitude of the DC signal
-// -- delay             := Number of samples the DC signal is delayed from time 0
-// INTERMEDIATE VARIABLES:
-// -- dcSignalSource    := Pointer to first element of signal
-// -- tSignal           := Temporary pointer used to offset the signal during stimulation
-// -- i, j              := Counters
-// RETURN:
-// -- < double > dcSignalSource
-// ERROR:
-// -- delay > size
-// TESTED: YES
-// RESULTS: PASSED
-*/
-double *gDSP_DCSignalGenerator( int size, double amplitude, int delay )
-{
-    double *dcSignalSource = malloc( (size_t)size ); // Allocate memory for signal
-    double *tSignal = dcSignalSource;
-    int i = 0;
-    int j = 0;
-
-    // Create delay in signal
-    for( j = 0; j < delay; j++ ) {
-            *tSignal = 0;
-            tSignal++;
-    } // END for
-
-    // Stimulates DC signal
-    for( i = 0; i < size; i++ ) {
-        *tSignal = amplitude;
-        tSignal++;
-    } // END for
-
-    return dcSignalSource;
-}
 //-----------------------------------------------------------------------------------------------------------
