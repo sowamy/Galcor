6dabdcfb3e0fa510b23da89bf6198ec2af99a440
Sowamy <adlefevers2@yahoo.com>
2017-04-29 10:52:10 -0500

    Update galcorTERMINAL.h

--- galcorTERMINAL.h
+++ galcorTERMINAL.h
@@ -1,4 +1,6 @@
 #ifndef GALCORGUI_H
 #define GALCORGUI_H
 void gTERMINAL_Start( void );
+void gTERMINAL_PrintIntegerArray( int *source, int size );
+void gTERMINAL_PrintFloatArray( float *source, int size );
 #endif // GALCORGUI_H
