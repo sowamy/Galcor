c70096d428c9d032c15340db44e502e8b9891fc6
Sowamy <adlefevers2@yahoo.com>
2017-05-02 12:35:29 -0500

    Delete galcorTERMINAL.h

--- galcorTERMINAL.h
+++
@@ -1,6 +0,0 @@
-#ifndef GALCORGUI_H
-#define GALCORGUI_H
-void gTERMINAL_Start( void );
-void gTERMINAL_PrintIntegerArray( int *source, int size );
-void gTERMINAL_PrintFloatArray( float *source, int size );
-#endif // GALCORGUI_H
