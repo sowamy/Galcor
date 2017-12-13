346b2b92d365f539c7e3fb97d37422009c029dd7
sowamy <adlefevers2@yahoo.com>
2017-12-13 06:50:54 -0600

    Structure PeriodicSignal is created

--- gDSP.c
+++ gDSP.c
@@ -4,3 +4,38 @@
  * 				and processes digital signals in a multitude of environments.
  */
 //-----------------------------------------------------------------------------------------------------------
+/* STRUCTURE: PeriodicSignal
+ * DESCRIPTION:
+ * -- This structure contains a periodic digital signal and it's attributes.
+ * MATHEMATICAL EQUATIONS:
+ * -- x(n + d) = A cos(wn + p), a < n < b
+ * -- -- x(n)	:= The amplitude of the signal at sample number n.
+ * -- -- n		:= The sample number.
+ * -- -- d		:= The amount of samples the signal is delayed from time 0.
+ * -- -- A		:= The (max) amplitude of the periodic signal.
+ * -- -- w		:= The frequency of the signal in radians per second.
+ * -- -- p		:= The phase shift of the signal in radians.
+ * -- -- a		:= The first sample in which the signal is described.
+ * -- -- b		:= The last sample in which the sample is described.
+ * VARIABLES:
+ * -- delay 					:= The number of samples that the signal is delayed from time 0.
+ * -- n 						:= The number of samples of which the signal consists.
+ * -- samplingTime 				:= The amount of time in microseconds between each sample.
+ * -- signal 					:= A pointer to the address of the first sample in the signal.
+ *									This pointer may be incremented to obtain each individual sample of the signal.
+ * -- amplitude 				:= Describes the highest and lowest samples in the signal.
+ * -- frequency_radPerSample	:= The frequency of the signal in radians per second.
+ * -- frequency_cyclesPerSample := The number of cycles of the signal per sample.
+ * -- phase						:= The phase shift of the signal in radians.
+ */
+struct PeriodicSignal {
+	int delay;
+	int n;
+	int samplingTime;
+	double *signal;
+	double amplitude = 0;
+	double frequency_radPerSample = 0;
+	double frequency_cyclesPerSample = 0;
+	double phase = 0; // In radians
+}; // END STRUCTURE --
+//-----------------------------------------------------------------------------------------------------------
