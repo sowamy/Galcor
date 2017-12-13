d5832ae500a28ff43c90ed0bd2cdc2de03776a8b
sowamy <adlefevers2@yahoo.com>
2017-12-13 06:29:07 -0600

    Commenting convention changes

--- gDSP.c
+++ gDSP.c
@@ -1,1 +1,6 @@
-
+//-----------------------------------------------------------------------------------------------------------
+/* FILE: gDSP.c
+ * DESCRIPTION: This file contains functionality and data structures which describes
+ * 				and processes digital signals in a multitude of environments.
+ */
+//-----------------------------------------------------------------------------------------------------------
